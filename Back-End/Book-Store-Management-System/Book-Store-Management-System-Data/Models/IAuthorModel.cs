3d6b4450921897d58cac823bc929a92ea0af57c2
Dragomir <bieltdrago@gmail.com>
2020-01-22 00:56:55 +0200

    Added interfaces for Genre and Author, added some code for testing the service.

---
+++ Back-End/Book-Store-Management-System/Book-Store-Management-System-Data/Models/IAuthorModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Book_Store_Management_System_Data.Models
+{
+    /// <summary>
+    /// How an author should be presented in the database.
+    /// </summary>
+    public interface IAuthorModel : IIdentifiable
+    {
+        /// <summary>
+        /// Gets or sets the name of the author.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets navigation property.
+        /// </summary>
+        ICollection<BookModel> Books { get; set; }
+    }
+}
